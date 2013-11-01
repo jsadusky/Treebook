ef0617223e0c19ef07420283c9f49f1520bc49d7
Jim Sadusky <jim@redguava.com.au>
2013-11-01 13:53:35 +1100

    created a new play rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'aa5216c4efc611fae09a2721ab6c826d1f8c9bdb469e9cb950c443a56b45d80017bc1803108dc5a0666130fc93546abd1894ef95234640759d2818581e2df49d'
